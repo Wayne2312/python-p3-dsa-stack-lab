909c5280f9d3a60b4b3aad3964f4cdef43bf0485
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-05-26 10:35:19 +0300

    Final

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
-
+    
     def __init__(self, items = [], limit = 100):
-        pass
-
+        self.limit = limit
+        self.items = items
+        self.list = items if items else []
+        
+        
     def isEmpty(self):
-        pass
+        return len(self.list) == 0
 
     def push(self, item):
-        pass
+        if len(self.list) >= self.limit:
+             raise Exception("Stack overflow: Cannot push item, stack is full.")
+        self.list.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.list.pop()
 
     def peek(self):
-        pass
-    
+        if self.isEmpty():
+            raise Exception("Stack is empty: Cannot peek.")
+        return self.list[-1]
+        
     def size(self):
-        pass
+        return len(self.list)
 
     def full(self):
-        pass
+        return len(self.list) >= self.limit
 
     def search(self, target):
-        pass
+        if target in self.list:
+            return len(self.list) -1- self.list.index(target)
+        return -1
